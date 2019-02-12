69e664865133ba01bfd77e9a0a31f15554df2a5e
suwenxiong <suwenxiong@meituan.com>
2019-02-12 11:25:58 +0800

    test: add single-pattern case

---
+++ test/single-pattern.test.js
@@ -0,0 +1,12 @@
+import test from 'ava'
+import requestMonitor from '../src/index'
+
+
+test('timeout test', t=>{
+  let result = requestMonitor.handleDefaultApi(p=> p)
+  t.is(result, true)
+
+  result = requestMonitor.handleDefaultApi(p=> p)
+  t.is(result, null)
+})
+
